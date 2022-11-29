4fef29b47102c7f2dc15cebffb5e7a6ed11e6365
Ian Mitchard <ian@Ians-MBP-2.home>
2022-11-29 09:01:53 -0700

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import Popular from './components/Popular'
+import "./index.css"
+
+/*
+ Component needs to: 
+ 1. maintain its own state
+ 2. dictate lifecycle
+ 3. render to the ui
+*/
+
+class App extends React.Component {
+    render () {
+        return (
+        <div className='light'>
+            <div className='container'>
+                <Popular />
+            </div>
+        </div>
+    )}
+}
+
+const rootElement = document.getElementById('app')
+const root = ReactDOM.createRoot(rootElement)
+root.render(<App />)
